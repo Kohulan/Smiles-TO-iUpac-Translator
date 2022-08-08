125970b84dceb39a814c577c79461c85c6209adb
Kohulan Rajan <kohulan.rajan@uni-jena.de>
2022-08-08 13:51:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     license="MIT",
     install_requires=[
-        "tensorflow-gpu==2.9.0",
+        "tensorflow==2.9.0",
         "pystow",
         "unicodedata2",
         "rdkit-pypi",
